25a6c9aa12a9a20270a9db6cd38a0434c2a8bec8
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-02-28 00:07:03 +0100

    Some code cleaning in bench-information_gain.R

--- inst/benchmarks/bench-information_gain.R
+++ inst/benchmarks/bench-information_gain.R
@@ -1,9 +1,8 @@
 library(FSelector)
 library(FSelectorRcpp)
 library(dplyr)
-library(RWeka)
 
-dt = lapply(1:5, function(xx)
+dt = lapply(1:500, function(xx)
 {
   x = rnorm(1000, mean = 10 * xx)
   y = rnorm(1000, mean = 0.5 * xx)
@@ -17,8 +16,4 @@ dt$z = as.factor(as.integer(round(dt$z)))
 
 system.time(information.gain(z ~ ., dt))
 system.time(information_gain(z ~ ., dt))
-
-all(as.numeric(Discretize(z ~ x, dt)[,1]) == fs_discretize(dt$x, dt$z) + 1)
-
-iris$Sepal.Length[3] = NA
 
