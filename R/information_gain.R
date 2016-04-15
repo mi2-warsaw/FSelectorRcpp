fea82927089af14db2b9f67043a545655e326053
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-04-15 20:29:35 +0200

    First attempt to sparse matrix

--- R/information_gain.R
+++ R/information_gain.R
@@ -39,6 +39,40 @@ information_gain = function(formula, data, type = c("infogain", "gainratio", "sy
   attrEntropy  = values$entropy
   jointEntropy = values$joint
 
+  results = information_type(classEntropy, attrEntropy, jointEntropy, type)
+
+  data.frame(importance = results, row.names = formula$x)
+}
+
+
+#' Entropy-based filters
+#'
+#' Entropy-based filters
+#'
+#' @param x sparse matrix
+#' @param y dependent variable
+#' @param type method name.
+#'
+#' @return data.frame
+#' @examples
+#'
+#' library(Matrix)
+#' i <- c(1,3:8); j <- c(2,9,6:10); x <- 7 * (1:7)
+#' x <- sparseMatrix(i, j, x = x)
+#' y = c(1,1,1,1,2,2,2,2)
+#'
+sp_information_gain = function(x, y, type = c("infogain", "gainratio", "symuncert"))
+{
+  type = match.arg(type)
+  values = sparse_information_gain_cpp(x,y)
+}
+
+
+information_type = function(classEntropy,
+                            attrEntropy,
+                            jointEntropy,
+                            type = c("infogain", "gainratio", "symuncert"))
+{
   results = classEntropy + attrEntropy - jointEntropy
 
   if(type == "gainratio")
@@ -49,6 +83,5 @@ information_gain = function(formula, data, type = c("infogain", "gainratio", "sy
     results = 2 * results / (attrEntropy	+ classEntropy)
   }
 
-
-  data.frame(importance = results, row.names = formula$x)
+  results
 }
