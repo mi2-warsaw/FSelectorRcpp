2c1138e84203e162f9fe26beccbf53be97612aa5
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-04-25 22:22:04 +0200

    Test for cutoff #14

---
+++ tests/testthat/test-cutoff.R
@@ -0,0 +1,19 @@
+library(Rcpp)
+library(RcppArmadillo)
+library(FSelector)
+
+test_that("Cutoff - test",
+{
+  # Prepare datasets
+  testDataFrame <- data.frame(x = rnorm(100000))
+  rownames(testDataFrame) <- paste0("row_",  1:100000)
+
+
+  # Testing C++ version:
+  x <- cutOff_k(rownames(testDataFrame), testDataFrame$x, k = 1894)
+
+  # Testing regular FSelector
+  x2 <- FSelector::cutoff.k(testDataFrame, 1894)
+
+  expect_equal(x, x2)
+})
