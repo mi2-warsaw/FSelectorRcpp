b7398fa74587782dfee908c88a7c3ba7d7fa0a69
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-05-23 22:39:10 +0200

    get_children for backward and forward search #19

--- R/utils.R
+++ R/utils.R
@@ -60,3 +60,74 @@ print.ExhaustiveSearchResult = function(x, ...)
   You can get them by rerunning fs_exhaustive_search with keepAll = TRUE\n")
   }
 }
+
+#### create children ----
+get_children = function (parent, direction = c("forward", "backward", "both"),
+          omit.func = NULL)
+{
+  # adopted from FSelector package
+
+  direction = match.arg(direction)
+  if (!is.null(omit.func))
+  {
+    omit.func = match.fun(omit.func)
+  }
+
+  cols = length(parent)
+  if (cols <= 0)
+    stop("Parent attribute set cannot be empty.")
+
+  m1 = NULL
+  m2 = NULL
+
+  if (direction == "forward" || direction == "both")
+  {
+    rows = cols - sum(parent)
+    if (rows > 0)
+    {
+      m1 = matrix(parent, ncol = cols, nrow = rows, byrow = TRUE)
+      CurrRow = 1
+      CurrCol = 1
+      while(CurrCol <= cols || CurrRow <= rows)
+      {
+        if (m1[CurrRow, CurrCol] == 0) {
+          m1[CurrRow, CurrCol] = 1
+          CurrRow = CurrRow + 1
+        }
+        CurrCol = CurrCol + 1
+      }
+    }
+  }
+
+  if (direction == "backward" || direction == "both")
+  {
+    rows = sum(parent)
+    if (rows > 1) {
+      m2 = matrix(parent, ncol = cols, nrow = rows, byrow = TRUE)
+      CurrRow = 1
+      CurrCol = 1
+      while(CurrCol <= cols || CurrRow <= rows)
+      {
+        if (m2[CurrRow, CurrCol] == 1)
+        {
+          m2[CurrRow, CurrCol] = 0
+          CurrRow = CurrRow + 1
+        }
+        CurrCol = CurrCol + 1
+      }
+    }
+  }
+
+  m = rbind(m1, m2)
+  if (is.null(m))
+    return(m)
+  if (!is.null(omit.func))
+  {
+    RowToOmit = apply(m, 1, omit.func)
+    return(m[!RowToOmit, , drop = FALSE])
+  } else
+  {
+    return(m)
+  }
+}
+
