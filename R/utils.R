77b4230924bd456f2772213e374a9523a01aa9b7
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-05-23 23:10:35 +0200

    Fix bug in get_children

--- R/utils.R
+++ R/utils.R
@@ -88,7 +88,7 @@ get_children = function (parent, direction = c("forward", "backward", "both"),
       m1 = matrix(parent, ncol = cols, nrow = rows, byrow = TRUE)
       CurrRow = 1
       CurrCol = 1
-      while(CurrCol <= cols || CurrRow <= rows)
+      while(CurrCol <= cols && CurrRow <= rows)
       {
         if (m1[CurrRow, CurrCol] == 0) {
           m1[CurrRow, CurrCol] = 1
@@ -106,7 +106,7 @@ get_children = function (parent, direction = c("forward", "backward", "both"),
       m2 = matrix(parent, ncol = cols, nrow = rows, byrow = TRUE)
       CurrRow = 1
       CurrCol = 1
-      while(CurrCol <= cols || CurrRow <= rows)
+      while(CurrCol <= cols && CurrRow <= rows)
       {
         if (m2[CurrRow, CurrCol] == 1)
         {
