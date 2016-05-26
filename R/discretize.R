8c5fb749afc2b4119556a751fee3246c1a060c5b
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-05-26 14:34:34 +0200

    Simple inteface for formula in discretize function (but we can rewrite something to use parallel computations:))

--- R/discretize.R
+++ R/discretize.R
@@ -11,6 +11,8 @@
 #'
 #' discretize(iris[[1]], iris[[5]])
 #'
+#' discretize(Species ~ ., iris)
+#'
 discretize = function(x, y)
 {
   UseMethod("discretize", x)
@@ -28,11 +30,37 @@ discretize.numeric = function(x, y)
   discretize_cpp(x, y)
 }
 
+#' @export
 discretize.formula = function(x, y)
 {
-  stop("Not implemented yet!")
+  formula = FSelectorRcpp:::formula2names(x, y)
+
+  data = y
+  yy   = y[[formula$y]]
+
+  colClasses = sapply(data,  is.numeric)
+  colClasses = colClasses[formula$x]
+
+  if(!all(colClasses))
+  {
+    warning(sprintf("Columns with classes other than numeric will be skipped! \n
+ Skipped columns:
+  %s",paste(names(colClasses)[!colClasses], collapse = ", ")))
+
+    colClasses = colClasses[colClasses]
+  }
+
+  columnsToDiscretize = names(colClasses)
+
+  for(col in columnsToDiscretize)
+  {
+    data[[col]] = discretize(data[[col]], yy)
+  }
+
+  return(data)
 }
 
+#' @export
 discretize.data.frame = function(x, y)
 {
   stop("Not implemented yet!")
