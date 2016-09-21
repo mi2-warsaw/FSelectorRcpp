bc83e8d8ad8a53ebb00df969e4f38d4c76415086
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-09-21 15:57:42 +0200

    Merge entopy.cpp, table.cpp with support.cpp

--- inst/benchmarks/bench-entropy.R
+++ inst/benchmarks/bench-entropy.R
@@ -2,11 +2,6 @@ library(FSelectorRcpp)
 library(entropy)
 library(microbenchmark)
 
-
-x = rnorm(1000,mean = 1000)
-microbenchmark(entropy(x), fs_numeric_entropy(x))
-
-
 x = as.integer(rnorm(1000, mean = 1000, sd = 5))
-microbenchmark(entropy(table(x)), fs_entropy1d(x))
+microbenchmark(entropy(table(x)), FSelectorRcpp:::fs_entropy1d(x))
 
