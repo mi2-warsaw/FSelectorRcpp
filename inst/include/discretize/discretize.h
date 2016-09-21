5391747a21892519f29a39975acb404f13b3506e
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-09-21 19:42:09 +0200

    Global discretize function

--- inst/include/discretize/discretize.h
+++ inst/include/discretize/discretize.h
@@ -3,4 +3,39 @@
 
 #include "discretize/methods/mdl.h"
 
+namespace fselector
+{
+
+namespace discretize
+{
+
+enum DISCRETIZE_METHOD {
+  MDL = 0,
+  QUANTILE = 1
+};
+
+template<class InputIterator,
+         class VariableIterator,
+         class OutputIterator>
+         std::vector<typename std::iterator_traits<InputIterator>::value_type> discretize(InputIterator itX,
+                                                                                          InputIterator itXLast,
+                                                                                          VariableIterator itY,
+                                                                                          OutputIterator itResult,
+                                                                                          DISCRETIZE_METHOD method = DISCRETIZE_METHOD::MDL)
+{
+  switch(method)
+  {
+    case DISCRETIZE_METHOD::MDL:
+      return mdl::discretize(itX, itXLast, itY, itResult);
+      break;
+
+  }
+
+
+}
+
+
+}
+}
+
 #endif
