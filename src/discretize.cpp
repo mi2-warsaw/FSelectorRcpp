783189a0279d0e81c16baab6743a098dd81fa6ee
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-09-21 16:22:07 +0200

    Remove unnecessary code

--- src/discretize.cpp
+++ src/discretize.cpp
@@ -5,41 +5,6 @@
 // [[Rcpp::plugins(cpp11)]]
 using namespace Rcpp;
 
-// [[Rcpp::export]]
-NumericVector fs_cut_index(const NumericVector& x, const IntegerVector& y)
-{
-
-  fselector::discretize::OptPair result = fselector::discretize::cut_index(x.begin(), x.end(), y.begin(), y.end());
-  NumericVector res(2);
-
-  if(result)
-  {
-    res[0] = result->first + 1;
-    res[1] = result->second;
-  }
-
-  return res;
-}
-
-// [[Rcpp::export]]
-double fs_mdl_stop(const int& ci, const IntegerVector& y, const double& entropy)
-{
-  fselector::discretize::OptDouble res = fselector::discretize::mdl_stop(y.begin(), y.end(), ci, entropy);
-
-  if(res) return *res;
-  return 0.0;
-}
-
-// [[Rcpp::export]]
-NumericVector fs_part(const NumericVector& x, const IntegerVector& y)
-{
-  std::set<int> splitPoints;
-
-  fselector::discretize::part(x.begin(), x.end(), y.begin(), y.end(), 0, 1.0, splitPoints);
-
-  return wrap(splitPoints);
-
-}
 
 // [[Rcpp::export]]
 IntegerVector discretize_cpp(const NumericVector& x, const IntegerVector& y)
