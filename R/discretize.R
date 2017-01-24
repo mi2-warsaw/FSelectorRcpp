e3666ade92e95b6e516c1dbd7a905a6a6238d861
krzyslom <krzysztofslomczynski@gmail.com>
2017-01-24 15:23:31 +0100

    #26 Google's R Style Guide and output unification for different methods

--- R/discretize.R
+++ R/discretize.R
@@ -1,16 +1,16 @@
 #' @export
-mdlControl = function()
-{
-  params = list(method = "MDL")
-  attr(params, "class") = c("mdlControl", "discretizationControl", "list")
+mdlControl <- function() {
+  params <- list(method = "MDL")
+  attr(params, "class") <- c("mdlControl", "discretizationControl", "list")
   params
 }
 
 #' @export
-equalsizeControl = function(k = 10)
-{
-  params = list(method = "EQUAL_SIZE", k = k)
-  attr(params, "class") = c("equalsizeControl", "discretizationControl", "list")
+equalsizeControl <- function(k = 10) {
+  params <- list(method = "EQUAL_SIZE", k = k)
+  attr(params, "class") <- c("equalsizeControl",
+                             "discretizationControl",
+                             "list")
   params
 }
 
@@ -52,82 +52,79 @@ equalsizeControl = function(k = 10)
 #' @author Zygmunt Zawadzki , \email{zygmunt.zawadzki@@gmail.com}
 #'
 #' @export
-discretize = function(x, y, control = mdlControl(), keepAll = FALSE)
-{
+discretize <- function(x, y, control = mdlControl(), keepAll = FALSE) {
   UseMethod("discretize", x)
 }
 
 #' @export
-discretize.default = function(x, y, control = mdlControl(), keepAll = FALSE)
-{
+discretize.default <- function(x, y, control = mdlControl(), keepAll = FALSE) {
   stop(sprintf("Object of class %s is not supported!", class(x)[1]))
 }
 
 #' @export
-discretize.numeric = function(x, y, control = mdlControl(), keepAll = FALSE)
-{
-  call = match.call()
-  res = discretize_cpp(x, y, control)
+discretize.formula <- function(x, y, control = mdlControl(), keepAll = FALSE) {
+  formula <- formula2names(x, y)
 
-  if(!is.null(attr(res,"SplitValues")))
-  {
-    # ini case of no split points
-    splitVals = attr(res, "SplitValues")
-    levels(res) = levels(cut(splitVals,splitVals))
+  data <- y
+  yy <- y[[formula$y]]
+
+  colClasses <- sapply(data, is.numeric)
+  colClasses <- colClasses[formula$x]
+
+  if (all(!colClasses)) {
+    stop("No columns of numeric classes!")
+  } else if (!all(colClasses)) {
+    warning(paste("Columns with classes other than numeric will be skipped!\n",
+                  "\n",
+                  " Skipped columns:",
+                  paste(names(colClasses)[!colClasses], collapse = ", ")))
+
+    colClasses <- colClasses[colClasses]
   }
 
-  dt  = data.frame(res, y, stringsAsFactors = FALSE)
-  colnames(dt) = c(call$x, call$y)
-  dt
-}
+  columnsToDiscretize <- names(colClasses)
 
-#' @export
-discretize.formula = function(x, y, control = mdlControl(), keepAll = FALSE)
-{
-  formula = formula2names(x, y)
+  for (col in columnsToDiscretize) {
+    res <- discretize_cpp(data[[col]], yy, control)
 
-  data = y
-  yy   = y[[formula$y]]
-
-  colClasses = sapply(data,  is.numeric)
-  colClasses = colClasses[formula$x]
-
-  if(!all(colClasses))
-  {
-    warning(sprintf("Columns with classes other than numeric will be skipped! \n
- Skipped columns:
-  %s",paste(names(colClasses)[!colClasses], collapse = ", ")))
-
-    colClasses = colClasses[colClasses]
-  }
-
-  columnsToDiscretize = names(colClasses)
-
-  for(col in columnsToDiscretize)
-  {
-    res = discretize_cpp(data[[col]], yy, control)
-
-    if(!is.null(attr(res,"SplitValues")))
-    {
+    if (!is.null(attr(res,"SplitValues"))) {
       # ini case of no split points
-      splitVals = attr(res, "SplitValues")
-      levels(res) = levels(cut(splitVals,splitVals))
+      splitVals <- attr(res, "SplitValues")
+      levels(res) <- levels(cut(splitVals,splitVals))
     }
 
-    data[[col]] = res
+    data[[col]] <- res
   }
 
-  if(!keepAll)
-  {
-    data = data[,c(formula$x, formula$y)]
+  if (!keepAll) {
+    data <- data[, c(columnsToDiscretize, formula$y)]
   }
 
   return(data)
 }
 
 #' @export
-discretize.data.frame = function(x, y, control = mdlControl())
-{
-  stop("Not implemented yet!")
+discretize.data.frame <- function(x, y, call = match.call(),
+                                  control = mdlControl(), keepAll = FALSE) {
+  if (!is.data.frame(y)) {
+    y <- format_handler(call$y, y)
+  }
+  y <- cbind(y, x)
+  if (any(table(colnames(y)) != 1)) {
+    stop("Duplicated columns!")
+  }
+  x <- formula(paste0(colnames(y)[1], "~."))
+
+  discretize.formula(x = x, y = y, control = control, keepAll = keepAll)
 }
 
+#' @export
+discretize.numeric <- function(call, x, y, control = mdlControl(), keepAll = FALSE) {
+  call <- match.call()
+  x <- format_handler(call$x, x)
+  discretize.data.frame(call = call, x = x, y = y, control = control,
+                        keepAll = keepAll)
+}
+
+#' @export
+discretize.list <- discretize.numeric
