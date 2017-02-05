e04a8c1588dc0bb22fe056c545f27113cc1bdf0e
krzyslom <krzysztofslomczynski@gmail.com>
2017-02-05 18:05:07 +0100

    #13 Unified output for f_search

--- R/param_search.R
+++ R/param_search.R
@@ -1,253 +1,73 @@
 utils::globalVariables("it")
 
-#' Exhaustive search
-#'
-#' The algorithm for searching atrribute subset space.
-#'
-#' @param attributes character vector with attributes names
-#' @param fun function to evaluate. See Details.
-#' @param data data set for fun function.
-#' @param subsetsSizes sizes of attributes subsets.
-#' @param singleAttr singleAttr
-#' @param keepAll keepAll
-#' @param allowParallel allowParallel,
-#' @param randomSubsetsNumber number of random subsets. Used only if greater than zero.
-#' @param \dots other arguments passed to foreach
-#'
-#' @importFrom foreach foreach %dopar% %do%
-#' @importFrom iterators iter
-#' @examples
-#'
-#'
-#' ### enable parallelization in examples
-#'
-#'  library(doParallel)
-#'  cl = makeCluster(2)
-#'  registerDoParallel(cl)
-#' # close at the end
-#' # stopCluster(cl)
-#' # registerDoSEQ()
-#'
-#' ####################################
-#' ## 1) evaluator from FSelector package
-#' ####################################
-#' evaluator = function(subset, data)
-#' {
-#'   library(rpart)
-#'   k <- 5
-#'   splits <- runif(nrow(data))
-#'   results = sapply(1:k, function(i) {
-#'   test.idx <- (splits >= (i - 1) / k) & (splits < i / k)
-#'   train.idx <- !test.idx
-#'   test <- data[test.idx, , drop=FALSE]
-#'   train <- data[train.idx, , drop=FALSE]
-#'   tree <- rpart(to_formula(subset, "Species"), train)
-#'   error.rate = sum(test$Species != predict(tree, test, type="c")) / nrow(test)
-#'   return(1 - error.rate)
-#'    })
-#'   return(mean(results))
-#' }
-#'
-#'
-#'  system.time(exhaustive_search(names(iris)[-5], evaluator, iris))
-#'  system.time(exhaustive_search(names(iris)[-5],
-#'                                evaluator,
-#'                                iris,
-#'                                allowParallel = FALSE))
-#'  system.time(exhaustive_search(names(iris)[-5],
-#'                                evaluator,
-#'                                iris,
-#'                                allowParallel = FALSE,
-#'                                randomSubsetsNumber = 5))
-#'
-#'
-#'
-#' ################################
-#' ## 2) maximize R^2 statistics in
-#' ## the linear regression model/problem
-#' ################################
-#'
-#' evaluator_R2_lm = function(attributes, data, dependent = names(iris)[1])
-#' {
-#'   summary(
-#'     lm(to_formula(attributes, dependent), data = data)
-#'   )$r.squared
-#' }
-#'
-#'
-#' system.time(exhaustive_search(names(iris)[-1], fun = evaluator_R2_lm, data =iris))
-#'
-#'
-#'
-#' ##############################
-#' ## 3) optimize BIC crietion in
-#' ## generalized linear model
-#' ##
-#' ## Aim of Bayesian approach it to identify the
-#' ## model with the highest probability of being
-#' ## the true model. - Kuha 2004
-#' ##############################
-#' utils::data(anorexia, package = "MASS")
-#'
-#' evaluator_BIC_glm <- function(attributes, data, dependent = "Postwt")
-#' {
-#'   extractAIC(
-#'     glm(to_formula(attributes, dependent),
-#'         family = gaussian, data = data),
-#'     k = log(nrow(data))
-#'   )[2]
-#' }
-#'
-#' exhaustive_search(c("Prewt", "Treat", "offset(Prewt)"),
-#'                   fun = evaluator_BIC_glm,
-#'                   data = anorexia)
-#'
-#' ### close parallelization
-#' stopCluster(cl)
-#' registerDoSEQ()
-#'
-#' @importFrom foreach getDoParRegistered
-#' @export
-#'
+#' @noRd
 exhaustive_search <- function(attributes, fun, data,
-                              subsetsSizes = length(attributes),
-                              singleAttr = FALSE, keepAll = TRUE,
-                              allowParallel = TRUE, randomSubsetsNumber = 0,
-                              ...) {
+                              subsetsSizes = 1:length(attributes),
+                              allowParallel = TRUE, ...) {
+
   len <- length(attributes)
-  if (len == 0) {
-    stop("Attributes length must be > 0.")
-  }
   fun <- match.fun(fun)
-
-  bestVal <- -Inf
-  bestAttr <- NULL
-
-  allCombn <- NULL
-  allResult <- NULL
-
-  if (length(subsetsSizes) == 1 & !singleAttr) {
-    subsetsSizes <- 1:subsetsSizes
-  }
-
-  `%op%` <- ifelse(allowParallel && getDoParRegistered(), `%dopar%`, `%do%`)
+  zeroes <- rep(0, len)
+  childComb <- NULL
 
   for (size in subsetsSizes) {
-    if (randomSubsetsNumber > len) {
-      if (choose(len, size) <= randomSubsetsNumber) {
-        childComb <- combn(1:len, size) # use all subsets
-      } else {
-        childComb <- replicate(randomSubsetsNumber,
-                              {sample.int(len, size, replace = FALSE)})
+    childComb <- cbind(childComb,
+                       apply(combn(1:len, size), 2, function(i) {
+                         x <- zeroes
+                         x[i] <- 1
+                         x
+                       }))
+  }
+
+  matIter <- iter(childComb, by = "column")
+
+  if (allowParallel && getDoParRegistered()) {
+    `%op%` <- `%dopar%`
+    if (getDoParName() == "doSNOW") {
+      tpb <- txtProgressBar(max = matIter$length, style = 3)
+      progress <- function(n) {
+        setTxtProgressBar(pb = tpb, value = n)
       }
+      opts <- list(progress = progress)
     } else {
-      childComb <- combn(1:len, size)
+      opts <- NULL
     }
-
-    zeroes <- rep(0, len)
-    childComb <- apply(childComb, 2, function(i) {
-      x <- zeroes
-      x[i] <- 1
-      x
-    })
-    childComb <- t(childComb)
-
-    matIter <- iter(childComb, by = "row")
-
-    result <- foreach(it = matIter, ...) %op% {
-      fun(attributes[as.logical(as.numeric(it))], data)
-    }
-
-    result <- unlist(result)
-
-    if (keepAll) {
-      allResult <- c(allResult, result)
-      allCombn <- rbind(allCombn, childComb)
-    }
-
-    maxIdx = which.max(result)
-
-    if (result[maxIdx] > bestVal) {
-      bestVal <- result[maxIdx]
-      bestAttr <- childComb[maxIdx,]
-    }
-  }
-
-  if (keepAll) {
-    res <- list(result = bestVal, bestAttr = bestAttr,
-               allResult = cbind(allCombn, values = allResult),
-               attributes = attributes)
   } else {
-    res <- list(result = bestVal, bestAttr = bestAttr, attributes = attributes)
+    `%op%` <- `%do%`
+    opts <- NULL
   }
 
-  attr(res, "class") <- c("ExhaustiveSearchResult", "list")
+  result <- foreach(it = matIter, .options.snow = opts, ...) %op% {
+    fun(attributes[as.logical(as.numeric(it))], data)
+  }
+
+  maxIdx <- which.max(result)
+  allResults <- setNames(as.data.frame(t(rbind(childComb, result))),
+                         c(attributes, "values"))
+  bestResult <- allResults[maxIdx, ]
+  res <- list(bestResult = bestResult,
+              allResults = allResults,
+              fun = fun)
 
   return(res)
 }
 
-#### greedy search ----
-
-#' Greedy Search
-#'
-#' The algorithm for searching atrribute subset space.
-#'
-#' @param attributes character vector with attributes names
-#' @param fun function to evaluate. See Details.
-#' @param data data set for fun function.
-#' @param type algorithm used for search. Possible values \code{forward} and \code{backward}.
-#' @param allowParallel allowParallel
-#' @param \dots other arguments passed to foreach
-#'
-#' @examples
-#'
-#' evaluator = function(subset, data)
-#' {
-#'   library(rpart)
-#'   k <- 5
-#'   splits <- runif(nrow(data))
-#'   results = sapply(1:k, function(i) {
-#'   test.idx <- (splits >= (i - 1) / k) & (splits < i / k)
-#'   train.idx <- !test.idx
-#'   test <- data[test.idx, , drop=FALSE]
-#'   train <- data[train.idx, , drop=FALSE]
-#'   tree <- rpart(to_formula(subset, "Species"), train)
-#'   error.rate = sum(test$Species != predict(tree, test, type="c")) / nrow(test)
-#'   return(1 - error.rate)
-#'    })
-#'   return(mean(results))
-#'  }
-#'
-#' library(doParallel)
-#' cl = makeCluster(2)
-#' registerDoParallel(cl)
-#'
-#' system.time(greedy_search(names(iris)[-5], evaluator, iris))
-#' system.time(greedy_search(names(iris)[-5], evaluator, iris, allowParallel = FALSE))
-#'
-#' stopCluster(cl)
-#' registerDoSEQ()
-#'
-#' @export
+#' @noRd
 greedy_search <- function(attributes, fun, data,
-                          type = c("forward", "backward"), allowParallel = TRUE,
-                          ...) {
-  if (length(attributes) == 0) {
-    stop("Attributes not specified.")
-  }
+                          type = c("forward", "backward"),
+                          allowParallel = TRUE, ...) {
 
+  len <- length(attributes)
   fun <- match.fun(fun)
   type <- match.arg(type)
-
   isForward <- type == "forward"
+  best <- list(result = -Inf, attrs = rep(as.numeric(!isForward), len))
 
-  best = list(result = -Inf, attrs = rep(as.numeric(!isForward),
-                                         length(attributes)))
   if (!isForward) {
-    best$result = fun(attributes[as.logical(best$attrs)], data)
+    best$result <- fun(attributes[as.logical(best$attrs)], data)
   }
 
-  `%op%` = ifelse(allowParallel && getDoParRegistered(), `%dopar%`, `%do%`)
+  allResults <- NULL
 
   repeat {
     children <- get_children(best$attrs, type)
@@ -257,21 +77,169 @@ greedy_search <- function(attributes, fun, data,
 
     iterChild <- iter(children, by = "row")
 
-    childrenResults <- foreach(it = iterChild, ...) %op% {
-      fun(attributes[as.logical(it)], data)
+    if (allowParallel && getDoParRegistered()) {
+      `%op%` <- `%dopar%`
+      if (getDoParName() == "doSNOW") {
+        tpb <- txtProgressBar(max = iterChild$length, style = 3)
+        progress <- function(n) {
+          setTxtProgressBar(pb = tpb, value = n)
+        }
+        opts <- list(progress = progress)
+      } else {
+        opts <- NULL
+      }
+    } else {
+      `%op%` <- `%do%`
+      opts <- NULL
     }
 
-    childrenResults <- unlist(childrenResults)
+    childrenResults <- foreach(it = iterChild, .options.snow = opts, ...) %op% {
+      fun(attributes[as.logical(as.numeric(it))], data)
+    }
 
     maxIdx <- which.max(childrenResults)
 
-    if (childrenResults[maxIdx] > best$result) {
-      best$result <- childrenResults[maxIdx]
+    if (childrenResults[[maxIdx]] > best$result) {
+      best$result <- childrenResults[[maxIdx]]
       best$attrs <- children[maxIdx, ]
+      allResults <- rbind(allResults, c(best$attrs, best$result))
     } else {
       break
     }
   }
 
-  best
+  allResults <- setNames(as.data.frame(allResults),
+                         c(attributes, "values"))
+  bestResult <- tail(allResults, 1)
+  res <- list(bestResult = bestResult,
+              allResults = allResults,
+              fun = fun)
+
+  return(res)
+}
+
+#' General search function
+#'
+#' Convenience function wrapper for greedy and exhaustive algorithms for
+#' searching atrribute subset space.
+#'
+#' @param attributes Character vector with attributes names.
+#' @param fun Function to evaluate. See Details.
+#' @param data Data set for fun function.
+#' @param mode String to determine which search to perform
+#' @param type Argument for \code{mode = "greedy"}. Algorithm used for search.
+#' Possible values are \code{forward} and \code{backward}.
+#' @param subsetsSizes Argument for \code{mode = "exhaustive"}. Vector of sizes
+#' of attributes subsets.
+#' @param allowParallel Allow parallelization.
+#' @param \dots Other arguments passed to foreach function.
+#'
+#' @importFrom foreach foreach %dopar% %do% getDoParRegistered getDoParName
+#' @importFrom iterators iter
+#' @importFrom stats setNames
+#' @importFrom utils setTxtProgressBar txtProgressBar tail
+#' @examples
+#'
+#' # Enable parallelization in examples
+#'  library(doSNOW) # doSNOW has an option for progress bar
+#'  cl <- makeCluster(2)
+#'  registerDoSNOW(cl)
+#'
+#' # Close at the end
+#' # stopCluster(cl)
+#' # registerDoSEQ()
+#'
+#' # 1) Evaluator from FSelector package.
+#' evaluator <- function(subset, data, dependent = names(iris)[5]) {
+#'   library(rpart)
+#'   k <- 5
+#'   splits <- runif(nrow(data))
+#'   results <- sapply(1:k, function(i) {
+#'     test.idx <- (splits >= (i - 1) / k) & (splits < i / k)
+#'     train.idx <- !test.idx
+#'     test <- data[test.idx, , drop = FALSE]
+#'     train <- data[train.idx, , drop = FALSE]
+#'     tree <- rpart(to_formula(subset, dependent), train)
+#'     error.rate <- sum(test[[dependent]] != predict(tree, test, type = "c")) /
+#'     nrow(test)
+#'     return(1 - error.rate)
+#'   })
+#'   return(mean(results))
+#' }
+#'
+#' # Default greedy search.
+#' system.time(f_search(attributes = names(iris)[-5],
+#'                      fun = evaluator,
+#'                      data = iris))
+#' system.time(f_search(attributes = names(iris)[-5],
+#'                      fun = evaluator,
+#'                      data = iris,
+#'                      allowParallel = FALSE))
+#'
+#' # Optional exhaustive search.
+#' system.time(f_search(attributes = names(iris)[-5],
+#'                      fun = evaluator,
+#'                      data = iris,
+#'                      mode = "exhaustive"))
+#' system.time(f_search(attributes = names(iris)[-5],
+#'                      fun = evaluator,
+#'                      data = iris,
+#'                      mode = "exhaustive",
+#'                      allowParallel = FALSE))
+#'
+#' # 2) Maximize R^2 statistics in the linear regression model/problem.
+#'
+#' evaluator_R2_lm <- function(attributes, data, dependent = names(iris)[1]) {
+#'   summary(
+#'     lm(to_formula(attributes, dependent), data = data)
+#'   )$r.squared
+#' }
+#'
+#' f_search(attributes = names(iris)[-1], fun = evaluator_R2_lm, data = iris,
+#'          mode = "exhaustive")
+#'
+#' # 3) Optimize BIC crietion in generalized linear model.
+#' # Aim of Bayesian approach it to identify the model with the highest
+#' # probability of being the true model. - Kuha 2004
+#'
+#' utils::data(anorexia, package = "MASS")
+#'
+#' evaluator_BIC_glm <- function(attributes, data, dependent = "Postwt") {
+#'   extractAIC(
+#'     fit = glm(to_formula(attributes, dependent), family = gaussian,
+#'               data = data),
+#'     k = log(nrow(data))
+#'   )[2]
+#' }
+#'
+#' f_search(attributes = c("Prewt", "Treat", "offset(Prewt)"),
+#'          fun = evaluator_BIC_glm,
+#'          data = anorexia,
+#'          mode = "exhaustive")
+#'
+#' # Close parallelization
+#' stopCluster(cl)
+#' registerDoSEQ()
+#'
+#' @export
+f_search <- function(attributes, fun, data, mode = c("greedy", "exhaustive"),
+                     type = c("forward", "backward"),
+                     subsetsSizes = 1:length(attributes), allowParallel = TRUE,
+                     ...) {
+
+  call <- match.call()
+  mode <- match.arg(mode)
+  if (mode == "greedy") {
+    type <- match.arg(type)
+    output <- greedy_search(attributes = attributes, fun = fun, data = data,
+                            type = type, allowParallel = allowParallel, ...)
+  } else if (mode == "exhaustive") {
+    output <- exhaustive_search(attributes = attributes, fun = fun, data = data,
+                                subsetsSizes = subsetsSizes,
+                                allowParallel = allowParallel, ...)
+  }
+
+  output <- c(output, call = call)
+
+  return(output)
 }
