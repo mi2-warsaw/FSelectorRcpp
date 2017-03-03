118d5703c7700b65c70c0fb515bd0f3c9e2e258c
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2017-03-03 18:22:22 +0100

    Turn off rcpp tests

--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -2,7 +2,7 @@ context("C++")
 
 library(entropy)
 
-if(getRversion() != "3.4.0") {
+if(getRversion() > "3.4.0") {
 test_that("Catch unit tests pass", {
     expect_cpp_tests_pass("FSelectorRcpp")
 })
