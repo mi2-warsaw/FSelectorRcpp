7b7adca585ff2ac423dc1b82fa6e1392428701f6
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2017-06-30 07:42:28 +0200

    Add lintr to tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,5 +6,10 @@ library(Rcpp)
 library(RcppArmadillo)
 library(dplyr)
 library(entropy)
+library(lintr)
 
 test_check("FSelectorRcpp")
+
+linters <- default_linters
+linters$camel_case_linter <- NULL
+expect_lint_free(linters = linters)
