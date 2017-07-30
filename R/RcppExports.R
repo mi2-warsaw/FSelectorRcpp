2fe571f743dd6ac69cfddc2b67edde992783c75d
krzyslom <krzysztofslomczynski@gmail.com>
2017-07-30 20:13:11 +0200

    Adjust example for new info gain output. #64

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -3,20 +3,18 @@
 
 #' Selection Cutoffs for Ranked Attributes
 #'
-#' @param x1 Probably vector with attributes' names.
-#' @param x2 Probably vector with attributes' ranks fo cut off.
-#' @param k A numeric. For \code{k > 1} it indicates how many attributes to take
-#' with the highest attribute rank (chooses k best attributes). For \code{0 < k  =< 1} it stands for the percent
-#' of top attributes to take (chooses best k * 100% of attributes). For \code{k = 0} it selects top attributes
-#' before the biggest gap in differences of scores for neighbouring attributes
-#' (chooses a subset of attributes which are significantly better than other.)
+#' @param x1 Vector with attributes' names.
+#' @param x2 Vector with attributes' ranks for cut off.
+#' @param k A numeric. For \code{k >= 1} it indicates how many attributes to take
+#' with the highest attribute rank (chooses k best attributes). For \code{0 < k < 1} it stands for the fraction
+#' of top attributes to take (chooses best k * 100% of attributes).
 #'
 #' @author Damian Skrzypiec, \email{damian.j.skrzypiec@@gmail.com}
 #'
 #' @examples
 #'
-#' x = information_gain(Species ~ ., iris)
-#' cutOff_k(rownames(x), x[[1]], 2)
+#' x <- information_gain(Species ~ ., iris)
+#' cutOff_k(x[[1]], x[[2]], 2)
 #'
 #' @noRd
 cutOff_k <- function(x1, x2, k) {
