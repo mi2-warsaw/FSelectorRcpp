39d8ad453c79b180f8895122d3da5c4ef96fc16c
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-11-08 09:49:32 +0100

    remove unused code from cpp

--- src/discretize.cpp
+++ src/discretize.cpp
@@ -16,30 +16,11 @@ IntegerVector discretize_cpp(const NumericVector& x, const IntegerVector& y, con
                                     control);
   result = result + 1; //discretize returns values starting from zero
 
-  Rcpp::CharacterVector splitVals(splitPoints.size() + 1);
-
-  size_t i = 0;
-  std::string first = "(-Inf;";
-  std::string last  = "";
-  for(auto iter = splitPoints.begin(); iter != splitPoints.end(); iter++ )
-  {
-    const std::string spl  = std::to_string(*iter);
-    const std::string frmt = first + spl + "]";
-    first = std::string("(") + spl + ";";
-    splitVals[i] = frmt;
-    i++;
-  }
-
   if(splitPoints.empty())
   {
     IntegerVector x(1, IntegerVector::get_na());
     return x;
   }
-
-  splitVals[splitPoints.size()] = std::string("(") +
-                                    std::to_string(splitPoints.back()) + ";" + "Inf)";
-
-  result.attr("levels") = splitVals;
 
   Rcpp::NumericVector splitValues(splitPoints.begin(), splitPoints.end());
   splitValues.push_front(R_NegInf);
