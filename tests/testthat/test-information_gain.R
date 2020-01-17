cefac2db8fc0e362a9143558f2137caa53e181e4
Zygmunt Zawadzki <zygmunt@zstat.pl>
2020-01-17 20:01:08 +0100

    another test for information_gain with character vector

--- tests/testthat/test-information_gain.R
+++ tests/testthat/test-information_gain.R
@@ -208,5 +208,22 @@ test_that("Information gain - character column", {
   )
 
   expect_equal(information_gain(y ~ ., dt), expected)
+
+  dt2 <- dt
+  dt2[[2]][10:20] <- NA
+  dt2[[3]][15:25] <- NA
+
+  expectedNA <- structure(
+    list(
+      attributes = c("x", "z"),
+      importance = c(0.431330813628601,
+                     0.236289962850477)
+    ),
+    class = "data.frame",
+    row.names = c(NA,-2L)
+  )
+
+  expect_equal(information_gain(y ~ ., dt2), expectedNA)
+
 })
 
